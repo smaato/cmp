f61583040ca5c7c64b663af5183d445a9ea22c05
Marco Pajares Silva <marco.pajares@smaato.com>
2018-04-11 12:15:09 +0200

    No vendorConsents object

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -16,9 +16,8 @@ export default class App extends Component {
 		sendPortalCommand({
 			command: 'readVendorConsent',
 		}).then(result => {
-			let obj = JSON.stringify(this.props.store.getVendorConsentsObject().vendorConsents);
-			console.log('Read consent data from global cookie', result + obj);
-			window.location = `consent://${result}/${obj}`;
+			console.log('Read consent data from global cookie', result);
+			window.location = `consent://${result}`;
 		}).catch(err => {
 			log.error('Failed reading global vendor consent cookie', err);
 		});
