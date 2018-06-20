ad3fd0492b7acefaea8870c8d4691a4d7e50c54d
Marco Pajares Silva <marco.pajares@smaato.com>
2018-06-20 17:57:09 +0200

    consent URL function triggered from config

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,5 @@
 import { h, Component } from 'preact';
 import style from './app.less';
-import { sendPortalCommand } from '../lib/portal';
 import Popup from './popup/popup';
 import Footer from './footer/footer';
 
@@ -12,14 +11,6 @@ export default class App extends Component {
 	onSave = () => {
 		const { store, notify } = this.props;
 		store.persist();
-		// Fetch all information we need from the cookie
-		sendPortalCommand({
-			command: 'readVendorConsent',
-		}).then(result => {
-			window.location = `consent://${result ? result : ''}`;
-		}).catch(err => {
-			log.error('Failed reading global vendor consent cookie', err);
-		});
 		notify('onSubmit');
 		store.toggleConsentToolShowing(false);
 	};
